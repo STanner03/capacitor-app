b702d960d730129d2dd3950fcca8f34934c48525
Shane <shanetanner03@gmail.com>
2023-01-09 20:08:21 -0600

    Created APP. + Imported Capacitor IOS & ANDROID.

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
+import Login from "./routes/Login";
+import Dashboard from "./routes/Dashboard";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route path="" element={<Login />}></Route>
+          <Route path="/app/dashboard" element={<Dashboard />}></Route>
+          <Route
+            path="*"
+            element={
+              <main style={{ padding: "1rem" }}>
+                <p>There's nothing here!</p>
+                <Link to="/">Back Home!</Link>
+              </main>
+            }
+          />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
